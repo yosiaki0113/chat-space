2da9ab313b7c9f4d20d2963128d2f3cd290555e1
yosiaki0113 <65077274+yosiaki0113@users.noreply.github.com>
2020-08-22 19:22:57 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
+    
   else  
     storage :fog
   end
